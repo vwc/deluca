f2b98113f50de8de15d3cdc4dfb85110015f340a
Christoph Boehner <cb@vorwaerts-werbung.de>
2012-03-30 15:14:52 +0200

    Fix the css errot and include the persistent tile registered for site root

--- src/deluca.sitecontent/deluca/sitecontent/specialsviewlet.py
+++ src/deluca.sitecontent/deluca/sitecontent/specialsviewlet.py
@@ -1,19 +1,17 @@
 from five import grok
 from Acquisition import aq_inner
-from zope.component import getMultiAdapter
+from zope.interface import Interface
 from plone.app.layout.viewlets.interfaces import IPortalHeader
 from Products.CMFCore.interfaces import IContentish
-from plone.app.layout.navigation.navtree import buildFolderTree
-from plone.app.layout.navigation.root import getNavigationRoot
-from Products.CMFPlone.browser.navtree import DefaultNavtreeStrategy
+from Products.CMFCore.interfaces import ISiteRoot
 
 
 class SpecialsViewlet(grok.Viewlet):
     grok.name('deluca.sitecontent.SpecialsViewlet')
-    grok.context(IContentish)
+    grok.context(Interface)
     grok.require('zope2.View')
     grok.viewletmanager(IPortalHeader)
 
     def update(self):
         context = aq_inner(self.context)
-        context_url = context.absolute_url()
+        self.context_url = context.absolute_url()
